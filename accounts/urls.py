d02258f2d5f29857ef43f403338b4d8b0f42e97c
Laila Aquino <lailaaquino8@gmail.com>
2026-04-29 11:39:52 -0300

    add URLs de paciente

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import CadastroPsicologoView, LoginUsuarioView, LogoutUsuarioView, PsicologoListView
+from .views import CadastroPsicologoView, LoginUsuarioView, LogoutUsuarioView, PsicologoListView, CadastroPacienteView, PacienteListView, inativar_paciente, PacienteUpdateView
 
 urlpatterns = [
     path("psicologos/", PsicologoListView.as_view(), name="psicologos"),
     path("psicologos/cadastrar/", CadastroPsicologoView.as_view(), name="cadastro_psicologo"),
     path("login/", LoginUsuarioView.as_view(), name="login"),
     path("logout/", LogoutUsuarioView.as_view(), name="logout"),
-    
+    path("pacientes/", PacienteListView.as_view(), name="pacientes_lista"),
+    path("pacientes/cadastrar/", CadastroPacienteView.as_view(), name="cadastro_paciente"),
+    path("pacientes/<uuid:pk>/inativar/", inativar_paciente, name="inativar_paciente"),
+    path("pacientes/<uuid:pk>/editar/", PacienteUpdateView.as_view(), name="editar_paciente"),
 ]
