7cd7f2bdfcfcedf03eb7dfa08f3f396b9cea78d0
Leticia Lopes <leticiazlzlima@gmail.com>
2026-05-15 23:12:35 -0300

    PSI-77: ajusta porta de email

--- psicoplus/settings.py
+++ psicoplus/settings.py
@@ -130,8 +130,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Configuração de E-mail via SMTP integrada com o Render
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 # Puxa as credenciais seguras do painel do Render
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
