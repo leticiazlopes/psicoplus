77afce4099897e3c0ac414fec167308646e46d32
Leticia Lopes <leticiazlzlima@gmail.com>
2026-05-31 16:19:49 -0300

    PSI-109: Configura banco persistido do deploy

--- psicoplus/settings.py
+++ psicoplus/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -68,10 +69,11 @@ WSGI_APPLICATION = 'psicoplus.wsgi.application'
 
 # DATABASE
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600,
+        ssl_require=True if os.environ.get('DATABASE_URL') else False
+    )
 }
 
 
